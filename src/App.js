77d2c085a50782d031cf7d78cd58303bf9c316e2
sacden <sacden2009@gmail.com>
2021-10-04 20:52:41 +0200

    feat: add basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import "./styles.css";
+import "./styles/styles.css";
 function App() {
-  return <div className="page"></div>;
+  return (
+    <div className="page">
+      <div className="header">
+        <div className="header__container">
+          <div className="header__logo"></div>
+          <div className="header__search-form"></div>
+          <div className="header__login"></div>
+        </div>
+      </div>
+      <div className="content"></div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 export default App;
