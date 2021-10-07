7a09b8c7a5f082593bf79abe53d1d7f4daed1807
sacden <sacden2009@gmail.com>
2021-10-07 16:23:10 +0200

    add sass styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import "./styles/styles.css";
 import Header from "./components/Main/Header/Header";
 import Content from "./components/Main/Content/Content";
 import Footer from "./components/Main/Footer/Footer";
+import Background from "../src/styles/img/wrap_bg.png";
 
 function App() {
   return (
-    <div className="page container-fluid">
+    <div
+      className="page container-fluid"
+      style={{
+        backgroundImage: `url(${Background})`,
+        minHeight: "100%",
+      }}
+    >
       <Header />
       <Content />
       <Footer />
--- src/components/Main/Content/Content.jsx
+++ src/components/Main/Content/Content.jsx
@@ -2,9 +2,11 @@ import Home from "../../../pages/Home/Home";
 import Login from "../../../pages/Login/Login";
 import Registration from "../../../pages/Registration/Registration";
 import { Route } from "react-router-dom";
+import classes from "./Content.module.scss";
+
 const Content = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/registration" component={Registration} />
